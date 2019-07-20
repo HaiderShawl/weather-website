5902c498c86f42d096941d6ff4bd4024ca3e5bb7
HaiderShawl <34603371+HaiderShawl@users.noreply.github.com>
2019-07-20 11:17:22 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -85,6 +85,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('The server is running on port 3000');
+app.listen(port, () => {
+    console.log('The server is running on port ' + port);
 });
