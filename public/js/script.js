6669533950b8c75760140494ba6cf4ea299a18de
HaiderShawl <34603371+HaiderShawl@users.noreply.github.com>
2019-07-20 10:21:00 +0530

    Initial Commit

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+console.log('Script is running')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const mes1 = document.querySelector('#m1')
+const mes2 = document.querySelector('#m2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                mes1.textContent = data.error
+            } else {
+                mes1.textContent = data.location
+                mes2.textContent = data.forecast
+            }
+        })
+    })
+})
+
---
+++ src/app.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+//defining paths
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+const publicPath = path.join(__dirname, '../public')
+
+
+const app = express()
+
+
+//setting handlebars and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialPath)
+
+
+app.use(express.static(publicPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Homepage',
+        name: 'Haider'
+    })
+});
+
+
+// app.get('/weather', (req, res) => {
+//     res.render('weather', {
+//         title: 'Weather',
+//         name: 'Haider',
+//         location: 'Srinagar',
+//         weather: 'Sunny'
+//     })
+// });
+
+
+app.get('/weather', (req, res) => {
+    address = req.query.address
+    if (!address) {
+        return res.send({
+            error: 'You must provide an address',
+        })
+    }
+
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            return res.send({
+                location,
+                forecast: forecastData,
+            })
+            
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: 'Error Page',
+        errorMessage: 'Help article not found',
+        name: 'Haider'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: 'Error Page',
+        errorMessage: 'Page not found',
+        name: 'Haider'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('The server is running on port 3000');
+});
